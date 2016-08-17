07db1b55e2b7cdba64de876db12ecbb177b1b1f8
Ethan Puzarne <epuzarne@wespire.com>
2016-08-17 11:46:49 -0400

    remove files that arent being used

--- js/video.js
+++
@@ -1,9 +0,0 @@
-function newVideo(sourceData) {
-  switch(sourceData.source) {
-    case 'youtube':
-      return newYoutubeVideo(sourceData.source_data);
-    default:
-      console.log('Unexpected video source ' + sourceSite);
-      return null;
-  }
-}
